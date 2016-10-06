46aea6e59dad8f1be0cb007d2163e578f1e19a8c
HamedSiasi <hamed@siasi.co.uk>
2016-10-06 10:41:19 +0100

    Ethernet

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
+/*
+ * PackageLicenseDeclared: Apache-2.0
+ * Copyright (c) 2015 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "mbed.h"
-#include "TCPSocket.h"
+//#include "TCPSocket.h"
 #include "EthernetInterface.h"
 
+
 DigitalOut led1(LED1);
-
 EthernetInterface eth;
-TCPSocket socket;
-
+//TCPSocket socket;
 
 int main() {
 
@@ -17,14 +33,12 @@ int main() {
     printf("IP address is: %s\r\n", ip ? ip : "No IP");
     printf("MAC address is: %s\r\n", mac ? mac : "No MAC");
 
-    
-    
-    
+    /*
     socket.open(&eth);
     socket.connect("developer.mbed.org", 80);
 
     // Send a simple http request
-    char sbuffer[] = "GET / HTTP/1.1\r\nHost: httpbin.org/ip\r\n\r\n";
+    char sbuffer[] = "GET / HTTP/1.1\r\nHost: developer.mbed.org\r\n\r\n";
     int scount = socket.send(sbuffer, sizeof sbuffer);
     printf("sent %d [%.*s]\r\n", scount, strstr(sbuffer, "\r\n")-sbuffer, sbuffer);
 
@@ -35,11 +49,13 @@ int main() {
 
     // Close the socket to return its memory and bring down the network interface
     socket.close();
-    eth.disconnect();
+    */
 
+
+    eth.disconnect();
     printf("Done\r\n");
     
-    //Blinky
+    //BlinkyLoop
     while (true) {
         led1 = !led1;
         Thread::wait(500);
